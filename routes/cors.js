01092e61373bd73fbc0a7da104091ec12db705c5
hanhnguyenhong18022000 <64351493+hanhnguyenhong18022000@users.noreply.github.com>
2021-03-01 21:51:35 +0700

    change port

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
+const whitelist = ['http://localhost:4000', 'http://localhost:4001'];
 var corsOptsDelegate = (req, callback) => {
     corsOpts = { origin: false };
     if(whitelist.indexOf(req.header('Origin')) !== -1) {
