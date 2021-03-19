9b678e83e9ac733f641c1749ee67c41130a17773
hanhnguyenhong18022000 <64351493+hanhnguyenhong18022000@users.noreply.github.com>
2021-03-19 09:37:13 +0700

    change db

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	'secretKey': '12345-67890-54321-098765',
-	'mongoUrl' : 'mongodb+srv://hanh-nh_18:123321@cluster0.vyhic.mongodb.net/travel_webDB'
-}
--- routes/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	'secretKey': '12345-67890-54321-098765',
-	'mongoUrl' : 'mongodb://localhost:27017/hanh',
-}
